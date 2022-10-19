553e3a6af9a173eb6cb903a9f09ddf0f7fbc74d6
David <pablodavidhuamani@gmail.com>
2022-10-19 17:26:57 -0500

    Component-Home
    Component Home, Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { BannerHiComponent } from './banner-hi/banner-hi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    BannerHiComponent
   ],
   imports: [
     BrowserModule,
